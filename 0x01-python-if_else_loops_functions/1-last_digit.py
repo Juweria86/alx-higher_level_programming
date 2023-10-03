6fa13a2ae55d6685be41030960a71ee7c386f30c
Juweria86 <jujumelmi143@gmail.com>
2023-10-03 08:46:14 +0300

    prints last digit og number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
-if last_digit < 0:
-    last_digit = -last_digit
+last_digit = (abs(number) % 10 * -1)
 print("Last digit of {} is {} and is".format(number, last_digit), end=" ")
 if last_digit > 5:
     print("greater than 5")
