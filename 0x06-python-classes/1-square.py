a0c8fa18431451e8f0d563e4282fd4a222145252
Juweria86 <jujumelmi143@gmail.com>
2023-10-25 11:41:28 +0300

    initializes square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """"""
+    def __init__(self, size):
+        """initializes class
+        size: size of square
+        """
+        self._size = size
