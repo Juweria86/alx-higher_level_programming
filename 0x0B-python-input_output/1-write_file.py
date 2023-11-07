25b584d6e993f1c5265143afe46d393d44121ae5
Juweria86 <jujumelmi143@gmail.com>
2023-11-07 13:38:59 +0300

    writes into a text file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Tis module writes into a file"""
 
+
 def write_file(filename="", text=""):
     """writes string into a text file(UTF8)"""
     with open(filename, "w") as file:
