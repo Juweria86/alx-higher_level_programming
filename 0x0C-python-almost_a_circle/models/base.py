0fdeae92a4f92ecb2a1975951d4dcd8adced8ceb
Juweria86 <jujumelmi143@gmail.com>
2023-11-15 17:28:20 +0300

    json representation of string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,4 +31,6 @@ class Base:
         args:
         list_dictionaries: list of dictionaries
         """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
         return (json.dumps(list_dictionaries))
