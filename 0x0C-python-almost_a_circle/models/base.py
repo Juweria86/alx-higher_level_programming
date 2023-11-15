28d20561644523a2d5ee6a5c73c92b6f7c408a75
Juweria86 <jujumelmi143@gmail.com>
2023-11-15 18:54:08 +0300

    returns an instance with all attributes already set

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -61,6 +61,7 @@ class Base:
         else:
             return json.loads(json_string)
 
+    @classmethod
     def create(cls, **dictionary):
         """Return a class instantied from a dictionary of attributes.
         Args:
