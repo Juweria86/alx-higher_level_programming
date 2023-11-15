ff51eb12fbea91e4a0c13e026f43b89b8b27da27
Juweria86 <jujumelmi143@gmail.com>
2023-11-15 19:05:48 +0300

    returns a list of instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -74,6 +74,7 @@ class Base:
                 new = cls(1)
             new.update(**dictionary)
             return new
+
     @classmethod
     def load_from_file(cls):
         filename = cls.__name__ + ".json"
@@ -83,4 +84,3 @@ class Base:
                 return [cls.create(**d) for d in list_dicts]
         except IOError:
             return []
-
